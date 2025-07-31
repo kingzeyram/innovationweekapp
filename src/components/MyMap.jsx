7dc190fe3fd583edc9e62db9e2b584dadd697d04
ramsey <lungahoramsey2005@gmail.com>
2025-07-31 14:21:38 +0300

    m

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -4,23 +4,24 @@ import 'leaflet/dist/leaflet.css';
 const MyMap = () => {
     return (
 
-
-        <MapContainer
-            center={[-1.286389, 36.817223]} // Nairobi coordinates
-            zoom={11}
-            scrollWheelZoom={true}
-            style={{ height: '300px', width: '75%' }}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[-1.230643, 36.878174]}>
-                <Popup>
-                    KCA University, Ruaraka, Nairobi
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div className="flex justify-center items-center py-8">
+            <MapContainer
+                center={[-1.286389, 36.817223]} // Nairobi coordinates
+                zoom={11}
+                scrollWheelZoom={true}
+                style={{ height: '300px', width: '75%' }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[-1.230643, 36.878174]}>
+                    <Popup>
+                        KCA University, Ruaraka, Nairobi
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 };
 
